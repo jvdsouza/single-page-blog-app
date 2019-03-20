18ea96ab32495a8f71ab9cbf7412ff9423b25b4f
Jason D'souza <jvincentdsouza@gmail.com>
2019-03-20 17:57:49 +1100

    initial commit with base project using nextjs

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => {
+    return(
+        <div>
+            <h1>Single page blog - by me</h1>
+            <Link>blog post 1</Link>
+        </div>
+    )
+}
+
+export default Index
